1704953f787539cfe5ff0cf559d77ea96d12dc0a
Eunicekk <108565785+Eunicekk@users.noreply.github.com>
2025-11-07 10:32:20 +0900

    [add] P154539_뒤에있는큰수찾기_김윤

---
+++ 251107/P154539_뒤에있는큰수찾기_김윤.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+    let result = Array(numbers.length).fill(-1);
+    let stack = [];
+    
+    for (let i = numbers.length - 1; i >= 0; i--) {
+        while (stack.length > 0 && stack[stack.length - 1] <= numbers[i]) {
+            stack.pop();
+        }
+        
+        if (stack.length > 0) {
+            result[i] = stack[stack.length - 1];
+        }
+        
+        stack.push(numbers[i]);
+    }
+    
+    return result;
+}
